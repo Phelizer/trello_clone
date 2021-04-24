b5737580ef93791907cb26985e9a4d04a413fe8a
Alex <phelizer@ukr.net>
2021-04-24 15:49:03 +0300

    Enabled requirement for JWT globally for all requests, except login request

--- back-end/agilis-tabula/src/app.module.ts
+++ back-end/agilis-tabula/src/app.module.ts
@@ -4,10 +4,17 @@ import { BoardsModule } from './boards/boards.module';
 import { SectionsModule } from './sections&tasks/sections.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [BoardsModule, SectionsModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
